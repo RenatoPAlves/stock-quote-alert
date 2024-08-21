644c7cd8b9c579d80502669e596f09a5ae90f5ab
renatodpa <rdapa@hotmail.com>
2024-08-20 23:26:49 -0300

    Creation of .env file
    Criação do arquivo .env, para segurança da API_Key, incluindo .env no arquivo .gitignore .
    
    Disclamer : O arquivo .env será enviado para o repositório git a fim de ser possível a execução do código por terceiros.

--- Services/APIServices.cs
+++ Services/APIServices.cs
@@ -1,4 +1,5 @@
 using System.Net.Http.Headers;
+using dotenv.net;
 
 namespace APIServices
 {
@@ -7,6 +8,7 @@ class APIHandler
 {
     private HttpClient httpClient;
     private HttpRequestMessage httpRequestMessage;
+    private string API_Key;
     const string baseURL = "https://real-time-finance-data.p.rapidapi.com";
 
     public APIHandler()
@@ -17,13 +19,16 @@ class APIHandler
 
     public Task<HttpResponseMessage> GetStockQuote(string symbol, string market = "BVMF")
     {
+        DotEnv.Load();
+        var API_Key = Environment.GetEnvironmentVariable("RapidAPI_Key");
+        Console.WriteLine(API_Key);
         httpRequestMessage = new HttpRequestMessage
         {
             Method = HttpMethod.Get,
             RequestUri = new Uri($"{baseURL}/stock-quote?symbol={symbol}%3A{market}&language=en"),
             Headers =
             {
-                { "x-rapidapi-key", "b8c33ec124msh52c407e6a80545fp17d985jsnd4663d14422d" },
+                { "x-rapidapi-key", API_Key },
                 { "x-rapidapi-host", "real-time-finance-data.p.rapidapi.com" },
             },
         };
