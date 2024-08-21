130ad880fc06a9d4fe4dacf48f48e7b6c00acbb3
renatodpa <rdapa@hotmail.com>
2024-08-20 22:49:01 -0300

    Initial testing of API Request

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Http.Headers;
+var client = new HttpClient();
+var request = new HttpRequestMessage
+{
+	Method = HttpMethod.Get,
+	RequestUri = new Uri("https://real-time-finance-data.p.rapidapi.com/stock-quote?symbol=PETR4%3ABVMF&language=en"),
+	Headers =
+	{
+		{ "x-rapidapi-key", "b8c33ec124msh52c407e6a80545fp17d985jsnd4663d14422d" },
+		{ "x-rapidapi-host", "real-time-finance-data.p.rapidapi.com" },
+	},
+};
+using (var response = await client.SendAsync(request))
+{
+	response.EnsureSuccessStatusCode();
+	var body = await response.Content.ReadAsStringAsync();
+	Console.WriteLine(body);
+}
