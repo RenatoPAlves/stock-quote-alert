0d0a471a15e46bef15e1338d12f5031003da5dc1
renatodpa <rdapa@hotmail.com>
2024-08-20 23:29:17 -0300

    Update params to readonly, and remove API_Key param of APIHandler

--- Services/APIServices.cs
+++ Services/APIServices.cs
@@ -3,37 +3,37 @@ using dotenv.net;
 
 namespace APIServices
 {
-    
-class APIHandler
-{
-    private HttpClient httpClient;
-    private HttpRequestMessage httpRequestMessage;
-    private string API_Key;
-    const string baseURL = "https://real-time-finance-data.p.rapidapi.com";
-
-    public APIHandler()
+    class APIHandler
     {
-        this.httpClient = new HttpClient();
-        this.httpRequestMessage = new HttpRequestMessage();
-    }
+        private readonly HttpClient httpClient;
+        private HttpRequestMessage httpRequestMessage;
+        const string baseURL = "https://real-time-finance-data.p.rapidapi.com";
 
-    public Task<HttpResponseMessage> GetStockQuote(string symbol, string market = "BVMF")
-    {
-        DotEnv.Load();
-        var API_Key = Environment.GetEnvironmentVariable("RapidAPI_Key");
-        Console.WriteLine(API_Key);
-        httpRequestMessage = new HttpRequestMessage
+        public APIHandler()
         {
-            Method = HttpMethod.Get,
-            RequestUri = new Uri($"{baseURL}/stock-quote?symbol={symbol}%3A{market}&language=en"),
-            Headers =
-            {
-                { "x-rapidapi-key", API_Key },
-                { "x-rapidapi-host", "real-time-finance-data.p.rapidapi.com" },
-            },
-        };
+            this.httpClient = new HttpClient();
+            this.httpRequestMessage = new HttpRequestMessage();
+        }
 
-        return httpClient.SendAsync(httpRequestMessage);
+        public Task<HttpResponseMessage> GetStockQuote(string symbol, string market = "BVMF")
+        {
+            DotEnv.Load();
+            var API_Key = Environment.GetEnvironmentVariable("RapidAPI_Key");
+            Console.WriteLine(API_Key);
+            httpRequestMessage = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(
+                    $"{baseURL}/stock-quote?symbol={symbol}%3A{market}&language=en"
+                ),
+                Headers =
+                {
+                    { "x-rapidapi-key", API_Key },
+                    { "x-rapidapi-host", "real-time-finance-data.p.rapidapi.com" },
+                },
+            };
+
+            return httpClient.SendAsync(httpRequestMessage);
+        }
     }
-}
 }
